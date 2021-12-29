0df74763b71798c5809d42ca4fe8b857fa88b31b
Yasin Aydın <thediavall@gmail.com>
2021-12-30 01:58:17 +0300

    💄add reset scss and base ui elements

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,18 @@
 import "@src/styles/index.scss";
 
 function App() {
-  return <div>Hello boyos!</div>;
+  return <div className="wrapper">
+    <div className="container p-2">
+      <form>
+        <h1 className="is-centered">Zahir'in Vurdurduğu Form</h1>
+        <div className="input-group">
+          <label htmlFor="name">Zahir Kime Vurduruyor?</label>
+          <input id="name" type="text" placeholder="İpucu: Herkese" />
+        </div>
+        <button className="is-green">Kaydet</button>
+      </form>
+    </div>
+  </div>;
 }
 
 export default App;
