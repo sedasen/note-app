d8199842fc1cb01538569b1528d5fc81107b8fda
Yasin Aydın <thediavall@gmail.com>
2022-01-02 17:44:28 +0300

    💄change style of theme-changer

--- src/components/ThemeChanger/themeChanger.tsx
+++ src/components/ThemeChanger/themeChanger.tsx
@@ -1,11 +1,22 @@
-import "./theme-changer.scss"
+import { useState } from "react";
+import "./theme-changer.scss";
+import { ReactComponent as MoonSVG } from "@src/assets/emojis/moon.svg";
+import { ReactComponent as SunSVG } from "@src/assets/emojis/sun.svg";
 
 const ThemeChanger = () => {
+  const [theme, setTheme] = useState("dark");
+
+  function changeTheme() {
+    if (theme === "dark") setTheme("light");
+    else setTheme("dark");
+  }
+
   return (
-    <div className="theme-changer">
-      Theme
-    </div>
-  )
-}
+    <button className="theme-changer" onClick={changeTheme}>
+      {theme === "dark" && <MoonSVG />}
+      {theme === "light" && <SunSVG />}
+    </button>
+  );
+};
 
 export default ThemeChanger;
