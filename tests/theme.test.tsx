ecc6441ec7b1c24becc5cb5c5b9bd2cdbfdb709b
Seda Şen <sedaseenn@gmail.com>
2022-01-03 18:44:37 +0300

    ✅ add more theme changer tests

--- tests/theme.test.tsx
+++ tests/theme.test.tsx
@@ -7,6 +7,7 @@ import Adapter from "enzyme-adapter-react-17-updated";
 import App from "@src/app";
 import { store } from "@src/store/store";
 import ThemeChanger from "@src/components/ThemeChanger/ThemeChanger";
+import { actions as ThemeActions } from "@src/store/duck/theme.duck";
 
 // @ts-ignore
 Enzyme.configure({ adapter: new Adapter() });
@@ -20,6 +21,21 @@ describe("theming-system", () => {
     themeChanger.simulate("click");
     const changedTheme = store.getState().theme.type;
 
-    expect(changedTheme).not.equal(initialTheme);
+    expect(initialTheme).equal("dark");
+    expect(changedTheme).equal("light");
+  });
+
+  it("should switch the logo", () => {
+    const wrapper = Enzyme.mount(<App />);
+
+    const initialLogo = wrapper.find("h1.title > img").props().src;
+
+    store.dispatch(ThemeActions.switchTheme());
+    wrapper.update();
+    wrapper.render();
+
+    const changedLogo = wrapper.find("h1.title > img").props().src;
+
+    expect(changedLogo).not.equal(initialLogo);
   });
 });
